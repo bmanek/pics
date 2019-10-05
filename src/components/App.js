4fe40cbea28fbbee39c739fbf04224962e70e678
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-05 15:51:15 -0400

    in progress: API key handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
+
+const API_KEY = process.env.REACT_APP_ACCESS_KEY
+const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
 
 class App extends React.Component {
 
   onSearchSubmit(term) {
-     console.log(term)
+    // axios.get('https://api.unsplash.com/search/photos', {
+    //   params: {query: term},
+    //   headers: {
+    //     Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`
+    //   }
+    // })
+    console.log(term)
+    console.log("API: ", API_KEY)
   }
 
   render() {
