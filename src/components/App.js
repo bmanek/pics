af62899b62e107085217a047eb938bb0fb48a9cf
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-06 19:04:03 -0400

    refactor onSearchSubmit() to async/await syntax

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,15 @@ const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
 
 class App extends React.Component {
 
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: {query: term},
-      headers: {
-        Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`
-      }
-    }).then( (response) => {
-      console.log(response)
-    })
+  async onSearchSubmit(term) {
+    const response = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: {query: term},
+        headers: {
+          Authorization: `Client-ID ${API_KEY}`
+        }
+      })
+    console.log(response.data.results)
   }
 
   render() {
