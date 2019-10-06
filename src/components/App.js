edf6c456d70c49a06d2a122b44a729a693383b49
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-06 19:15:27 -0400

    refactor onSearchSubmit() to fix this.setState error

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,9 @@ const API_KEY = process.env.REACT_APP_ACCESS_KEY
 const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
 
 class App extends React.Component {
+  state = { images: [] }
 
-  async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
     const response = await axios
       .get('https://api.unsplash.com/search/photos', {
         params: {query: term},
@@ -17,13 +18,15 @@ class App extends React.Component {
           Authorization: `Client-ID ${API_KEY}`
         }
       })
-    console.log(response.data.results)
+
+    this.setState({ images: response.data.results})
   }
 
   render() {
     return(
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     )
   }
