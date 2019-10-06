b8c619cd061a4f5cd29b6f391131d2f67453772d
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-06 19:29:15 -0400

    move headers, baseURL from onSearchSubmit to api/unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar'
-
-require('dotenv').config()
-
-const API_KEY = process.env.REACT_APP_ACCESS_KEY
-const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
 
 class App extends React.Component {
   state = { images: [] }
 
   onSearchSubmit = async (term) => {
-    const response = await axios
-      .get('https://api.unsplash.com/search/photos', {
+    const response = await unsplash
+      .get('/search/photos', {
         params: {query: term},
-        headers: {
-          Authorization: `Client-ID ${API_KEY}`
-        }
       })
 
     this.setState({ images: response.data.results})
