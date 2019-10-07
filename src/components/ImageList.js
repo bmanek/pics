9590dc5f2758af72a765e65656a15c9c69865587
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-07 15:26:10 -0400

    add render of images to page

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const ImageList = (props) => {
   console.log(props.images)
-  return <div>ImageList</div>
+  const pictures = props.images.map( (image) => {
+    return <img alt={image.alt_description} src={image.urls.regular} />
+  })
+  return <div>{pictures}</div>
 }
 
 export default ImageList
