16b9ccd2ff9e797897ae5cb1a28d337dead9ff4d
bmanek <41875460+bmanek@users.noreply.github.com>
2019-10-07 16:00:13 -0400

    destructure ImageList, add key props

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 const ImageList = (props) => {
   console.log(props.images)
-  const pictures = props.images.map( (image) => {
-    return <img alt={image.alt_description} src={image.urls.regular} />
+  const pictures = props.images.map( ({id, alt_description, urls}) => {
+    return <img key={id} alt={alt_description} src={urls.regular} />
   })
   return <div>{pictures}</div>
 }
 
 export default ImageList
+
+
+// destructuring: ln 4-5 was
+// const pictures = props.images.map( (image) => {
+//   return <img key={image.id} alt={image.alt_description} src={image.urls.regular} />
